97867f0ee56e1a14add15470167ba91275e3f9f4
3599josephw <3599@holbertonschool.com>
2022-08-10 10:42:25 -0500

    size_t i declaration moved

--- 112-array_to_bst.c
+++ 112-array_to_bst.c
@@ -7,9 +7,10 @@
 bst_t *array_to_bst(int *array, size_t size) {
 
 	bst_t *root;
+	size_t i;
 
 	root = NULL;
-	for (size_t i = 0; i < size; i++) {
+	for (i = 0; i < size; i++) {
 		bst_insert(&root, array[i]);
 	}
 	return (root);
